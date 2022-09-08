5b9e681db70ae151cda828f7af8288d59f06fdce
comeauc <comeauc@umich.edu>
2022-09-08 13:20:52 -0400

    April tag calibration (in progress)

--- state_machine.py
+++ state_machine.py
@@ -1,10 +1,14 @@
 """!
 The state machine that implements the logic.
 """
+from cmath import sqrt
+from turtle import pos
 from PyQt4.QtCore import (QThread, Qt, pyqtSignal, pyqtSlot, QTimer)
 import time
 import numpy as np
 import rospy
+from scipy.spatial.transform import Rotation
+from apriltag_ros.msg import AprilTagDetectionArray
 
 class StateMachine():
     """!
@@ -36,7 +40,12 @@ class StateMachine():
                             [0.75*np.pi/2,   -0.5,      -0.3,      0.0,      np.pi/2],
                             [np.pi/2,         0.5,       0.3,      0.0,       0.0   ],
                             [0.0,             0.0,       0.0,      0.0,       0.0   ]]
+        self.tag_positions = np.array([[-250, -25, 0],
+                               [ 250, -25, 0],
+                               [ 250, 275, 0],
+                               [-250, 275, 0]]) # in world frame
 
+        self.apriltag_sub = rospy.Subscriber('/tag_detections', AprilTagDetectionArray, self.apriltag_callback)
 
     def set_next_state(self, state):
         """!
@@ -105,8 +114,6 @@ class StateMachine():
     def execute(self):
         """!
         @brief      Go through all waypoints
-        TODO: Implement this function to execute a waypoint plan
-              Make sure you respect estop signal
         """
         self.status_message = "State: Execute - Executing motion plan"
         self.current_state = "execute"
@@ -124,7 +131,46 @@ class StateMachine():
         self.current_state = "calibrate"
         self.next_state = "idle"
 
-        """TODO Perform camera calibration routine here"""
+        if len(self.latest_tags.detections) < 4:
+            self.status_message = "Not enough tags visible (%d of 4)" % len(self.latest_tags)
+            return
+        
+        # IDs: BL = 1, BR = 2, TR = 3, TL = 4 (BR unstable)
+        plane_positions = [0] * 4
+        for tag in self.latest_tags.detections:
+            id = tag.id[0]
+            position = np.array([tag.pose.pose.pose.position.x, tag.pose.pose.pose.position.y, tag.pose.pose.pose.position.z]) * 1000
+            if id == 1:
+                tag_position_in_world = self.tag_positions[id - 1]
+                tag_position_relative_to_camera = position
+            plane_positions[id - 1] = position
+
+        vec_4_to_1 = plane_positions[0] - plane_positions[3]
+        vec_4_to_1 = vec_4_to_1 / np.linalg.norm(vec_4_to_1)
+        vec_4_to_3 = plane_positions[2] - plane_positions[3]
+        vec_4_to_3 = vec_4_to_3 / np.linalg.norm(vec_4_to_3)
+        normal_vec = np.cross(vec_4_to_1,vec_4_to_3)
+        normal_vec = normal_vec / np.linalg.norm(normal_vec)
+        
+        # Rotation.from_rotvec(normal_vec)
+        # c1 = sqrt(normal_vec[0] ** )
+        rot_matrix = np.array([vec_4_to_1,
+                               normal_vec,
+                               vec_4_to_3])
+
+        H_rot_camera_to_tag_plane = np.block([[rot_matrix,np.zeros((3,1))],[np.zeros((1,3)),1]])
+        H_world_to_tag_plane = np.array([   [ 0,  -1,   0,   tag_position_in_world[0] ],
+                                            [ 1,   0,   0,   tag_position_in_world[1] ],
+                                            [ 0,   0,   1,   tag_position_in_world[2] ],
+                                            [ 0,   0,   0,     1 ]])
+        # H_camera_to_tag = np.array([[ 1,   0,   0,   tag_position_relative_to_camera[0] ],
+        #                             [ 0,  -1,   0,   tag_position_relative_to_camera[1] ],
+        #                             [ 0,   0,  -1,   tag_position_relative_to_camera[2] ],
+        #                             [ 0,   0,   0,     1 ]])
+        rospy.logerr("Shapes %s %s", rot_matrix.shape,tag_position_relative_to_camera.shape)
+        H_camera_to_tag = np.block([[rot_matrix,np.reshape(tag_position_relative_to_camera,(3,1))],[np.zeros((1,3)),1]])
+        H_world_to_camera = np.matmul(H_world_to_tag_plane, np.linalg.inv(H_camera_to_tag))
+        rospy.logerr("Matrix: \n%s",H_world_to_camera)
         self.status_message = "Calibration - Completed Calibration"
 
     """ TODO """
@@ -145,6 +191,9 @@ class StateMachine():
             self.status_message = "State: Failed to initialize the rxarm!"
             rospy.sleep(5)
         self.next_state = "idle"
+    
+    def apriltag_callback(self, tags):
+        self.latest_tags = tags
 
 class StateMachineThread(QThread):
     """!
